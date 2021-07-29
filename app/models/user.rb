e39b32c851418541a824148cd70f73fd0364fc63
polly0296 <0296polly@gmail.com>
2021-07-29 13:36:03 +0900

    バリデーション書き換え

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :birth_date, presence: true
-  validates :password,format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-end
+
+  validates :password, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+   end
+  end
