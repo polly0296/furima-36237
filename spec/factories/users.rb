7a38c039af4e92fe62fc65dff70e6295706e0586
polly0296 <0296polly@gmail.com>
2021-07-29 13:42:56 +0900

    Userのモデル単体テストの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
+
   factory :user do
-    
+    nickname {'testユーザー'}
+    email {Faker::Internet.free_email}
+    password = 'test12'
+    password {password}
+    password_confirmation {password}
+    last_name {'山田'}
+    first_name {'太郎'}
+    last_name_kana {'ヤマダ'}
+    first_name_kana {'タロウ'}
+    birth_date {'1990-01-01'}
   end
+
 end
