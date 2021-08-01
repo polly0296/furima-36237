be6da626aa17feee099bff19d268b2406defd08b
polly0296 <0296polly@gmail.com>
2021-08-01 22:19:18 +0900

    販売手数料・利益計算のJavaScript実装

---
+++ app/javascript/fee_calculation.js
@@ -0,0 +1,11 @@
+function calculation (){
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
+    const addPofitDom = document.getElementById("profit");
+    addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+  })
+};
+
+window.addEventListener('load', calculation);
