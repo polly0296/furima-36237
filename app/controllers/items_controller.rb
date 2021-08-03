f19aee647b96361c2e6197be647952176061806f
polly0296 <0296polly@gmail.com>
2021-08-03 16:44:57 +0900

    商品詳細ページに商品情報を表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,10 +18,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :fee_id, :source_id, :shipday_id,
-                                 :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :fee_id, :source_id, :shipday_id, :price).merge(user_id: current_user.id)
   end
 end
