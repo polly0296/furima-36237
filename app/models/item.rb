c23f79cce1ef48065b6e5afe7a4d4343f263e6cf
polly0296 <0296polly@gmail.com>
2021-08-26 21:54:12 +0900

    コメント機能　修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image, dependent: :destroy
   has_one :order
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
