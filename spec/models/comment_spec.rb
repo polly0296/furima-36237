5b6322ae3b46e0bd11b5f4073d40cb2653f4f8ed
polly0296 <0296polly@gmail.com>
2021-08-26 11:12:29 +0900

    コメント機能　修正

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメント投稿' do
+    context 'コメントが投稿できる場合' do
+      it 'textが存在していれば保存できる' do
+        expect(@comment).to be_valid
+      end
+    end
+    context 'コメントが投稿できない場合' do
+      it 'textが空では保存できない' do
+        @comment.text = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Textを入力してください")
+      end
+      it 'itemが紐付いていないと保存できない' do
+        @comment.item = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('Itemを入力してください')
+      end
+      it 'userが紐付いていないと保存できない' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('Userを入力してください')
+      end
+    end
+  end
 end
